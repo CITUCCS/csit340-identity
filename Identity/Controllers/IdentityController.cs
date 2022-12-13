0a480b24c59a727fea9075a154aaf6108467f50a
Jhon Christian Ambrad <jhonchristian.ambrad@cit.edu>
2022-12-13 17:10:47 +0800

    Add identity controller

---
+++ Identity/Controllers/IdentityController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IdentityController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetIdentity(string name)
+        {
+            return Ok(new IdentityDto { Name = name, Age = 51 });
+        }
+
+
+    }
+}
---
+++ Identity/IdentityDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Identity
+{
+    public class IdentityDto
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
